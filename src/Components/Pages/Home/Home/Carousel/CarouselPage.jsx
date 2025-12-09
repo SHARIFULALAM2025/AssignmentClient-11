2859f278428b0d19e0a8859a5d6002dafb398c6d
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2025-12-09 14:33:53 +0600

    carousel add

---
+++ src/Components/Pages/Home/Home/Carousel/CarouselPage.jsx
@@ -0,0 +1,89 @@
+import Button from '@mui/material/Button'
+import React, { useEffect, useState } from 'react'
+// import { AuthContext } from '../../../../Authentication/Auth/AuthContext/AuthContext'
+import { IoIosArrowForward } from 'react-icons/io'
+import SendIcon from '@mui/icons-material/Send'
+import { IoIosArrowBack } from 'react-icons/io'
+import { useNavigate } from 'react-router'
+
+const CarouselPage = () => {
+  const [newProperty, setNewProperty] = useState([])
+  // const { theme } = useContext(AuthContext)
+  const [current, setCurrent] = useState(0)
+  const handelNext = () => {
+    if (current + 3 < newProperty.length - 1) {
+      setCurrent(current + 3)
+    } else {
+      setCurrent(0)
+    }
+  }
+  const handelPrev = () => {
+    if (current === 0) {
+      setCurrent(Math.max(newProperty.length - 3, 0))
+    } else {
+      setCurrent(current - 3)
+    }
+  }
+  useEffect(() => {
+    fetch('http://localhost:5000/myCarousel').then(res => res.json())
+      .then(result => {
+      setNewProperty(result)
+    })
+
+  }, [])
+  const navigate = useNavigate()
+  const handelNavigate = () => {
+    navigate('/books')
+  }
+  return (
+    <div>
+      <div className="relative w-full overflow-hidden rounded-xl shadow-xl">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+          {newProperty.slice(current, current + 3).map((item) => (
+            <div key={item._id} className="relative">
+              <figure>
+                <img
+                  src={item.image}
+                  alt=""
+                  className="object-cover w-full h-64"
+                />
+              </figure>
+              <div className="absolute inset-0  flex flex-col justify-center items-center text-white text-center px-5 ">
+                <h1 className="text-3xl font-bold text-green-600">
+                  {item.title}
+                </h1>
+                <p className="text-2xl font-bold text-green-600">{item.des}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <button
+          className="absolute left-8 top-1/2 -translate-y-1/2 bg-white/70 hover:bg-green-500 text-black p-2 rounded-full"
+          onClick={handelPrev}
+        >
+          <IoIosArrowBack size={24}></IoIosArrowBack>
+        </button>
+        <button
+          className="absolute right-8 top-1/2 -translate-y-1/2 bg-white/70 hover:bg-green-500 text-black p-2 rounded-full"
+          onClick={handelNext}
+        >
+          <IoIosArrowForward size={24}></IoIosArrowForward>
+        </button>
+        <Button
+          onClick={handelNavigate}
+          sx={{ position: 'absolute' }}
+          className="top-46 right-32"
+          variant="contained"
+          endIcon={<SendIcon />}
+        >
+          All Books
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default CarouselPage
+
+
+
--- src/Components/Pages/Home/Home/Home.jsx
+++ src/Components/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import CarouselPage from './Carousel/CarouselPage';
 
 const Home = () => {
   return (
     <div>
-      <h1 className="">this is home page</h1>
+      <CarouselPage></CarouselPage>
     </div>
   );
 };
