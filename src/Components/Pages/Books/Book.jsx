07c7ce4e692227b15ec2da03c5cfd9a47a7c9d15
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2025-12-09 10:53:06 +0600

    sort compepete

--- src/Components/Pages/Books/Book.jsx
+++ src/Components/Pages/Books/Book.jsx
@@ -3,7 +3,7 @@ import ReusableCard from '../../ReusableFunction/ReusableCard'
 import { Link, useNavigate } from 'react-router'
 
 const Book = ({ item }) => {
-  const { image, BookName, Category, author } = item
+  const { image, BookName, Category, author,Price } = item
   const navigate = useNavigate()
   const handelDetails = (id) => {
     navigate(`/BookDetails/${id}`)
@@ -15,7 +15,7 @@ const Book = ({ item }) => {
         imageSrc={image}
         imgClass="object-cover w-full h-[400px]"
         title={BookName}
-        description={author}
+        description={Price}
         TitleClassName=""
         className="border p-2"
       ></ReusableCard>
--- src/Components/Pages/Books/Books.jsx
+++ src/Components/Pages/Books/Books.jsx
@@ -5,6 +5,10 @@ import Container from '../../Container/Container'
 import Book from './Book'
 
 import { useState } from 'react'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
 
 const Books = () => {
   const AxiosSecure = useAxiosSecure()
@@ -25,24 +29,56 @@ const Books = () => {
     )
     setFilterBook(result)
   }
-const displayBook=filterBook.length>0?filterBook:book
+  const [sortOrder, setSetOrder] = useState('')
+  const handleChange = (e) => {
+    setSetOrder(e.target.value)
+  }
+  const displayBook = filterBook.length > 0 ? filterBook : book
+  const sortBook = [...displayBook].sort((a, b) => {
+    if (sortOrder === 'low to high') {
+      return a.Price - b.Price
+    }
+    if (sortOrder === 'high to low') {
+      return b.Price -a.Price
+    }
+    return 0
+  })
   return (
     <div>
       <Container>
         <div className="flex justify-center items-center mt-2 mb-2">
           <input
             onChange={(e) => setSearchText(e.target.value)}
-            placeholder='search book name'
+            placeholder="search book name"
             type="search"
             className="py-1 px-2 outline-none border rounded"
           />
 
-          <button onClick={handelSearch} className="py-1 px-2 bg-amber-200 rounded">
+          <button
+            onClick={handelSearch}
+            className="py-1 px-2 bg-amber-200 rounded"
+          >
             search
           </button>
         </div>
-        <div className="grid grid-cols-5 gap-3">
-          {displayBook.map((item, index) => (
+        <div className="flex justify-end mb-2">
+          <FormControl sx={{width:"300px"}}>
+            <InputLabel id="demo-simple-select-label">Select price</InputLabel>
+            <Select
+              className='w-1/2'
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={sortBook}
+              label="Select price"
+              onChange={handleChange}
+            >
+              <MenuItem value="low to high">low to high</MenuItem>
+              <MenuItem value="high to low">high to low</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div className="grid md:grid-cols-5 gap-3">
+          {sortBook.map((item, index) => (
             <Book item={item} key={index}></Book>
           ))}
         </div>
