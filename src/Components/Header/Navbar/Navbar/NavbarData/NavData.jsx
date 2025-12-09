e2ef268eb24bf2f53f7f1c59fd6e43040a0e8311
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2025-12-09 11:11:41 +0600

    navbar correction

--- src/Components/Header/Navbar/Navbar/NavbarData/NavData.jsx
+++ src/Components/Header/Navbar/Navbar/NavbarData/NavData.jsx
@@ -1,8 +1,8 @@
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd'
-import AccountBalanceIcon from '@mui/icons-material/AccountBalance'
+
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import HomeIcon from '@mui/icons-material/Home'
-import LocalShippingIcon from '@mui/icons-material/LocalShipping'
+
 import AutoStoriesIcon from '@mui/icons-material/AutoStories'
 
 import LogOutButton from '../../../../Authentication/Logout/LogOutButton';
@@ -19,14 +19,9 @@ const pages = [
     Name: 'Books',
     icon: <AutoStoriesIcon></AutoStoriesIcon>,
   },
+ 
   {
     id: 3,
-    path: 'RequestDelivery',
-    Name: 'Request Delivery',
-    icon: <LocalShippingIcon></LocalShippingIcon>,
-  },
-  {
-    id: 4,
     path: 'dashboard',
     Name: 'Dashboard',
     icon: <DashboardIcon></DashboardIcon>,
--- src/Components/RoutePage/Route.jsx
+++ src/Components/RoutePage/Route.jsx
@@ -7,27 +7,15 @@ import Login from '../Authentication/Login/Login'
 import AuthenticationLayout from '../LayoutPage/AuthenticationLayout/AuthenticationLayout'
 import Private from '../PrivatePage/Private'
 import DashboardLayout from '../LayoutPage/DashboardLayout/DashboardLayout'
-
-
 import Invoice from '../LayoutPage/DashboardLayout/DashboardPage/Invoice'
-
-
-
-
-
-
 import Profile from '../Pages/Profile/Profile'
-
-import RequestDelivery from '../Pages/RequestDelivery/RequestDelivery'
 import Books from '../Pages/Books/Books'
-
 import MyOrder from '../LayoutPage/DashboardLayout/DashboardPage/MyOrder'
 import AddBook from '../LayoutPage/DashboardLayout/DashboardPage/AddBook'
 import MyBooks from '../LayoutPage/DashboardLayout/DashboardPage/MyBooks'
 import Order from '../LayoutPage/DashboardLayout/DashboardPage/Order'
 import ManageBook from '../LayoutPage/DashboardLayout/DashboardPage/ManageBook'
 import AllUser from '../LayoutPage/DashboardLayout/DashboardPage/AllUser'
-
 import EditBook from '../LayoutPage/DashboardLayout/DashboardPage/EditBook/EditBook'
 import BookDetails from '../Pages/BookDetails/BookDetails'
 import PaymentSuccess from '../Pages/Payment/PaymentSuccess'
@@ -52,10 +40,7 @@ const router = createBrowserRouter([
           </Private>
         ),
       },
-      {
-        path: 'RequestDelivery',
-        Component: RequestDelivery,
-      },
+
       {
         path: 'profile',
         Component: Profile,
